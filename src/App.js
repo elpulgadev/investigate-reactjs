c74977dc62e46ed9c983bd9126afac9da700b39a
phi <elpulga.dev2021@gmail.com>
2022-10-17 06:39:54 +0700

    investigate useRef

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import UseRef1 from "./components/useRef/example-01";
+// import UseRef2 from "./components/useRef/example-02";
+// import UseRef3 from "./components/useRef/example-03";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <UseRef1 />
+      {/* <UseRef2 /> */}
+      {/* <UseRef3 /> */}
+    </>
   );
 }
 
